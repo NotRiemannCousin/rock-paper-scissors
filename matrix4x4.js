13319c64326c9736c6d3df4d73231c01a6a22f82
Marcelo <marcelohenrique8822@gmail.com>
2024-08-26 21:19:49 -0300

    matrix4x4 class added.

--- matrix4x4.js
+++ matrix4x4.js
@@ -1,10 +1,19 @@
-class Matrix {
+class Matrix4x4 {
     constructor(m11, m12, m13, m14, m21, m22, m23, m24, m31, m32, m33, m34, m41, m42, m43, m44) {
         this.data = new Float32Array([m11, m12, m13, m14, m21, m22, m23, m24, m31, m32, m33, m34, m41, m42, m43, m44]);
     }
 
+    static zero() {
+        return new Matrix4x4(
+            0, 0, 0, 0,
+            0, 0, 0, 0,
+            0, 0, 0, 0,
+            0, 0, 0, 0
+        );
+    }
+
     static identity() {
-        return new Matrix(
+        return new Matrix4x4(
             1, 0, 0, 0,
             0, 1, 0, 0,
             0, 0, 1, 0,
@@ -13,7 +22,7 @@ class Matrix {
     }
 
     static translation(x, y, z) {
-        return new Matrix(
+        return new Matrix4x4(
             1, 0, 0, x,
             0, 1, 0, y,
             0, 0, 1, z,
@@ -24,7 +33,7 @@ class Matrix {
     static rotationX(angle) {
         const c = Math.cos(angle);
         const s = Math.sin(angle);
-        return new Matrix(
+        return new Matrix4x4(
             1, 0, 0, 0,
             0, c, -s, 0,
             0, s, c, 0,
@@ -35,7 +44,7 @@ class Matrix {
     static rotationY(angle) {
         const c = Math.cos(angle);
         const s = Math.sin(angle);
-        return new Matrix(
+        return new Matrix4x4(
             c, 0, s, 0,
             0, 1, 0, 0,
             -s, 0, c, 0,
@@ -46,7 +55,7 @@ class Matrix {
     static rotationZ(angle) {
         const c = Math.cos(angle);
         const s = Math.sin(angle);
-        return new Matrix(
+        return new Matrix4x4(
             c, -s, 0, 0,
             s, c, 0, 0,
             0, 0, 1, 0,
@@ -55,11 +64,20 @@ class Matrix {
     }
 
     add(other) {
+        if (!(other instanceof Matrix4x4))
+            throw new Error("matrix4x4.add() only works with other matrix4x4");
+
+        if (Matrix4x4.zero() === other)
+            return this;
+
+        if (Matrix4x4.zero() === this) 
+            return other;
+
         const data = new Float32Array(16);
         for (let i = 0; i < 16; i++) {
             data[i] = this.data[i] + other.data[i];
         }
-        return new Matrix(...data);
+        return new Matrix4x4(...data);
     }
 
     "+"(other) {
@@ -67,6 +85,15 @@ class Matrix {
     }
 
     multiply(other) {
+        if (!(other instanceof Matrix4x4))
+            throw new Error("matrix4x4.multiply() only works with other matrix4x4");
+        
+        if (Matrix4x4.identity() === other)
+            return this;
+
+        if (Matrix4x4.identity() === this)
+            return other;
+
         const data = new Float32Array(16);
         for (let i = 0; i < 4; i++) {
             for (let j = 0; j < 4; j++) {
@@ -77,7 +104,7 @@ class Matrix {
                 data[i * 4 + j] = sum;
             }
         }
-        return new Matrix(...data);
+        return new Matrix4x4(...data);
     }
 
     "*"(other) {
@@ -85,4 +112,4 @@ class Matrix {
     }
 }
 
-export { Matrix };
+export { Matrix4x4 as Matrix };
